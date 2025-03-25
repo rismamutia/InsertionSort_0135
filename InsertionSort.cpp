5e16e40ccd4f26e5ca220e7f237128dcd51a6060
rismamutia <rismamutiadewi89@gmail.com>
2025-03-25 22:38:36 +0700

    menggunakan perintah While True

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,12 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
+
+void input()
+{ // Procedure Input
+while(true)
+{
+    cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+    cin >> n; // Memanggil variable inputan n
+}
+}
