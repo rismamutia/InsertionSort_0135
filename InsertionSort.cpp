a2892b0b9f20369a0c7b1e1225dc9a161a9f8e0a
rismamutia <rismamutiadewi89@gmail.com>
2025-03-25 22:51:24 +0700

    Menggunakan Variable for sebagai perulangan

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -24,4 +24,10 @@ cout << endl; //Membuat jarak per baris program
 cout << "======================" << endl; //Membuat tampilan susunan data element array
 cout << "Masukkan Element Array" <<endl;
 cout << "======================" << endl;
+
+for (int i = 0; i < n; i++) // Menggunakan Perulangan for untuk menyimpan data pada Array
+{
+    cout << "Data ke-" << (i + 1) << ": "; // Masukkan atau Menginputkan data pada Array
+    cin >> arr[i]; //Menyimpan nilai data n kedalam Array arr
+}
 }
