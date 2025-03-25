7fbe856f39e71271da865c6bd521446e54c05360
rismamutia <rismamutiadewi89@gmail.com>
2025-03-25 22:46:55 +0700

    fungsi untuk membuat jarak baris per program

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -20,5 +20,8 @@ while(true)
         cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
     }
 }
-
+cout << endl; //Membuat jarak per baris program
+cout << "======================" << endl; //Membuat tampilan susunan data element array
+cout << "Masukkan Element Array" <<endl;
+cout << "======================" << endl;
 }
